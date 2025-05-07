37c4d21dbf46483f4848b95cce32fb911902b9a1
luisvinatea <paulobarberena@gmail.com>
2025-05-07 18:42:54 -0300

    fix: update font loading logic to use Uint8List for better compatibility

--- source/conf.py
+++ source/conf.py
@@ -8,31 +8,32 @@ the AeraSync documentation using Sphinx.
 # -- Path setup --------------------------------------------------------------
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../backend'))
-sys.path.insert(0, os.path.abspath('../frontend/web'))
+
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../backend"))
+sys.path.insert(0, os.path.abspath("../frontend/web"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-PROJECT = 'AeraSync'
-COPYRIGHT = '2025, Luis Vinatea'
-AUTHOR = 'Luis Vinatea'
-RELEASE = '1.0.0'
+PROJECT = "AeraSync"
+COPYRIGHT = "2025, Luis Vinatea"
+AUTHOR = "Luis Vinatea"
+RELEASE = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
-    'sphinx_rtd_theme',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx_rtd_theme",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # Napoleon settings
 NAPOLEON_GOOGLE_DOCSTRING = True
@@ -52,15 +53,15 @@ NAPOLEON_ATTR_ANNOTATIONS = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-HTML_THEME = 'sphinx_rtd_theme'
-HTML_STATIC_PATH = ['_static']
+HTML_THEME = "sphinx_rtd_theme"
+HTML_STATIC_PATH = ["_static"]
 HTML_TITLE = "AeraSync Documentation"
-HTML_PROJECT = 'AeraSync'
-HTML_LOGO = 'icons/aerasync.webp'
-HTML_FAVICON = 'favicon.webp'
+HTML_PROJECT = "AeraSync"
+HTML_LOGO = "icons/aerasync.webp"
+HTML_FAVICON = "favicon.webp"
 
 # -- Intersphinx configuration ----------------------------------------------
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'flask': ('https://flask.palletsprojects.com/en/2.0.x/', None),
+    "python": ("https://docs.python.org/3", None),
+    "flask": ("https://flask.palletsprojects.com/en/2.0.x/", None),
 }
